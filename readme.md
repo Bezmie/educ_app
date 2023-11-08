a6016248651f56f6035a52793a4bcd7e5e83c5d0
NAME <bezmie@gmail.com>
2023-11-08 14:50:55 +0300

    1.0.1

--- readme.md
+++ readme.md
@@ -1,1 +1,42 @@
-test
+# Educ_app
+
+### Webpack:
+
+Common:
+``````
+  context => src
+  output  => dst
+        
+  clean dst            with 'CleanWebpackPlugin'
+  copy files from src  with 'CopyPlugin' to dst
+       
+  scss  => css
+  alias => @ = __dirname + src
+``````
+
+Serve environment:
+  ``````
+  webpack-dev-server with browser-sync
+  source-maps
+  ``````
+Prode environment:
+  ``````
+  minifies html,css,js
+  hashing file js, css
+  ``````
+
+### Needs
+  Node.js
+### Preps
+  ```
+  npm i
+  ```
+### Start
+Serve Mode
+  ```bash
+  npm run serve
+  ```
+Prode Mode
+  ```bash
+  npm run build
+  ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path')
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = !isProd
+const isProde = process.env.NODE_ENV === 'production'
+const isServe = !isProde
 
-const filename = ext => (
-  isDev && `bundle.${ext}` || `bundle.[hash].${ext}`
-)
+const filename = ext => (isServe && `bundle.${ext}` || `bundle.[hash].${ext}`)
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -17,34 +16,33 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dst')
   },
   resolve: {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
     }
   },
-  devtool: isDev && 'source-map' || false,
+  devtool: isServe && 'source-map' || false,
   devServer: {
     port: 3000,
-    hot: isDev
+    hot: isServe
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: 'index.html',
       minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd
+        removeComments: isProde,
+        collapseWhitespace: isProde
       }
     }),
     new CopyPlugin({
       patterns: [
         { 
           from: path.resolve(__dirname, 'src/favicon.svg'),
-          to: path.resolve(__dirname, 'dist')
+          to: path.resolve(__dirname, 'dst')
         }
       ]
     }),
@@ -61,16 +59,6 @@ module.exports = {
           "css-loader",
           "sass-loader"
         ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
       }
     ]
   }
